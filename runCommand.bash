e94cb91897b7b20f6c0ecf90296816a37ffba063
Jason Harrison <james.harrison@intel.com>
2022-04-28 07:43:23 -0700

    more test command after exit

--- runCommand.bash
+++ runCommand.bash
@@ -14,10 +14,10 @@ export PIGPIO_ADDR=soft
 export PIGPIO_PORT=8888
 pigpiod
 
-python -c "from gpiozero import LED; import pigpio; pigpio.pi(); relay = LED(37)"
 while true; do
     sleep 10
     
 exit 0
+python -c "from gpiozero import LED; import pigpio; pigpio.pi(); relay = LED(37)"
 
 python raiseDesk.py

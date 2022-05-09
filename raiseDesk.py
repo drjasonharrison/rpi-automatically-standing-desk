00d0f3cdd416742fdc8685d0643707e3e839ab66
Jason Harrison <james.harrison@intel.com>
2022-05-08 17:51:29 -0700

    use Relay_Module.py to fix raise_desk.py

--- raiseDesk.py
+++ raiseDesk.py
@@ -1,5 +1,8 @@
-from gpiozero import LED # The LED library allows easy pin control
-import pigpio
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
+
 from time import sleep
 import random
 import os
@@ -14,43 +17,48 @@ class RaiseDesk:
     def __init__(self):
         self.logger = logging.getLogger('RaiseDesk')
 
-        Relay_Ch1 = 26
-        Relay_Ch2 = 20
-        Relay_Ch3 = 21
+        self.Relay_Ch1 = 26
+        self.Relay_Ch2 = 20
+        self.Relay_Ch3 = 21
 
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
 
-        GPIO.setup(Relay_Ch1,GPIO.OUT)
-        GPIO.setup(Relay_Ch2,GPIO.OUT)
-        GPIO.setup(Relay_Ch3,GPIO.OUT)
+        GPIO.setup(self.Relay_Ch1, GPIO.OUT)
+        GPIO.setup(self.Relay_Ch2, GPIO.OUT)
+        GPIO.setup(self.Relay_Ch3, GPIO.OUT)
 
          # takes a long time for the desk to rise up
         self.pressRaiseDeskButtonSeconds = os.getenv("DESK_PRESS_BUTTON_SECONDS", 8)
         self.minMinutesBeforeRaisingDesk = os.getenv("DESK_MIN_PERIOD_MINUTES", 45)
         self.maxTimeBeforeRaisingDesk = os.getenv("DESK_MAX_PERIOD_MINUTES", 60)
 
+    def pressButton(self):
+        GPIO.output(self.Relay_Ch1, GPIO.LOW)
+
+    def releaseButton(self):
+        GPIO.output(self.Relay_Ch1, GPIO.LOW)
+
     def raiseDeskAndSleepForever(self):
         while True:
             ## if during "work hours" on a "work day"
-            self.relay.on()
+            self.pressButton()
             sleep(self.pressRaiseDeskButtonSeconds)
-            self.relay.off()
+            self.releaseButton()
             sleep(60 * random.randint(self.minMinutesBeforeRaisingDesk, self.maxTimeBeforeRaisingDesk) * self.maxTimeBeforeRaisingDesk)
 
-    def toggleRelayTest(self):
+    def toggleButtonTest(self):
         while True:
             print("on")
-            self.relay.on()
+            self.pressButton()
             sleep(2)
             print("off")
-            self.relay.off()
+            self.releaseButton()
             sleep(2)
 
 
     def main(self):
-        # self.init()
-        self.toggleRelayTest()
+        self.toggleButtonTest()
 
         self.raiseDeskAndSleepForever()
 

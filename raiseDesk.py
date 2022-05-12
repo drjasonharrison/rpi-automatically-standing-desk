7e5b749e887a7d08c1f39a8be1c0fb7557333dee
Jason Harrison <james.harrison@intel.com>
2022-05-12 08:45:23 -0700

    fix logging

--- raiseDesk.py
+++ raiseDesk.py
@@ -13,6 +13,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s %(name)s %(levelname)s %(message)s'
 )
+log = logging.getLogger("raiseDesk")
 
 class RaiseDesk:
     def __init__(self):
